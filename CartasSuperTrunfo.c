4cce4d548a3942ab22638db7e3c2a20c00eb0568
Flávio Vieira <jsv.flavio@gmail.com>
2025-08-21 04:40:50 +0000

    Criei o primeiro código do Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
@@ -6,17 +5,85 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+#include <stdio.h>
 
-    return 0;
+int main()
+{
+    char estado[20], codigo[10], cidade[20];    
+    int populacao, turisticos;
+    float km², pib;    
+//primeira parte (carta 01), variáveis, informações necessárias para alimentar a carta. 
+
+    printf("Código da carta: ");    
+    scanf("%s", codigo);
+
+    printf("Estado: ");
+    scanf("%s", estado);
+
+    printf("Cidade: ");
+    scanf("%s", cidade);
+
+    printf("População: ");
+    scanf("%d", &populacao);
+
+    printf("Qual o tamanho em km²?: ");
+    scanf("%f", &km²);
+
+    printf("Qual é o PIB?: ");
+    scanf("%f", &pib);
+
+    printf("Quantos pontos turísticos?: ");
+    scanf("%d", &turisticos);
+//segunda parte (carta 01), tipos de dados, indicando cada variável e seus valores.
+
+    printf("Código da carta: %s\n", codigo);
+    printf("Estado: %s\n", estado);
+    printf("Cidade: %s\n", cidade);
+    printf("População: %d\n", populacao);
+    printf("Com tamanho em km² de: %.2f\n", km²);
+    printf("PIB: %.2f\n", pib);
+    printf("Com %d pontos turísticos\n", turisticos);
+//terceira parte (carta 01), imprimindo na tela do usuário os valores inseridos.
+
+
+        char estado2[20], codigo2[10], cidade2[20];    
+        int populacao2, turisticos2;
+        float km²2, pib2;
+    //primeira parte (carta 02), variáveis, informações necessárias para alimentar a carta. 
+
+        printf("Código da carta: ");
+        scanf("%s", codigo2);
+
+        printf("Estado: ");
+        scanf("%s", estado2);
+
+        printf("Cidade: ");
+        scanf("%s", cidade2);
+
+        printf("População: ");
+        scanf("%d", &populacao2);
+
+        printf("Qual o tamanho em km²?: ");
+        scanf("%f", &km²2);
+
+        printf("Qual é o PIB?: ");
+        scanf("%f", &pib2);
+
+        printf("Quantos pontos turísticos?: ");
+        scanf("%d", &turisticos2);
+    //segunda parte (carta 02), tipos de dados, indicando cada variável e seus valores.
+
+        printf("Código da carta: %s\n", codigo2);
+        printf("Estado: %s\n", estado2);
+        printf("Cidade: %s\n", cidade2);
+        printf("População: %d\n", populacao2);
+        printf("Com tamanho em km² de: %.2f\n", km²2);
+        printf("PIB: %.2f\n", pib2);
+        printf("Com %d pontos turísticos\n", turisticos2);
+    //terceira parte (carta 02), imprimindo na tela do usuário os valores inseridos.
+
+
+return 0;
+
 }
+
