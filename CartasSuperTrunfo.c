e99cd914c0cbd0f54c55a53aa12e70355eabe82a
Flávio Vieira <jsv.flavio@gmail.com>
2025-09-04 23:52:38 +0000

    Novo commit, nível aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -40,8 +40,8 @@ int main()
     printf("Com tamanho de: %.2fkm²\n", km²);
     printf("Seu PIB é de: %.2f\n", pib);
     printf("Com %d pontos turísticos\n", turisticos);
-    printf("Com uma densidade demografica de: %.2f hab/km²\n", populacao / km²);//Desafio 2, Nível Intermediario
-    printf("Com uma renda per capta de: R$%.2f\n", pib / populacao);//Desafio 2, Nível Intermediario
+    printf("Com uma densidade demografica de: %.2f hab/km²\n", populacao / km²);//Desafio 2, Nível Aventureiro
+    printf("Com uma renda per capta de: R$%.2f\n", pib / populacao);//Desafio 2, Nível Aventureiro
 //terceira parte (carta 01), imprimindo na tela do usuário os valores inseridos.
 
 
@@ -82,8 +82,8 @@ int main()
         printf("Com tamanho de: %.2fkm²\n", km²2);
         printf("Seu PIB é de: %.2f\n", pib2);
         printf("Com %d pontos turísticos\n", turisticos2);
-        printf("Com uma densidade demografica de: %.2f hab/km²\n", populacao2 / km²2);//Desafio 2, Nível Intermediario
-        printf("Com uma renda per capta de: R$%.2f\n", pib2 / populacao2);//Desafio 2, Nível Intermediario
+        printf("Com uma densidade demografica de: %.2f hab/km²\n", populacao2 / km²2);//Desafio 2, Nível Aventureiro
+        printf("Com uma renda per capta de: R$%.2f\n", pib2 / populacao2);//Desafio 2, Nível Aventureiro
     //terceira parte (carta 02), imprimindo na tela do usuário os valores inseridos.
 
 
